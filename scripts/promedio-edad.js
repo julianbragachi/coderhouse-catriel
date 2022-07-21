e5991a7f5091f062b331f9c172b4593578b89745
julianbragachi <julian.bragachi@gmail.com>
2022-07-21 20:20:56 -0300

    desafio eventos

--- scripts/promedio-edad.js
+++ scripts/promedio-edad.js
@@ -1,3 +1,35 @@
+const form = document.querySelector("#form");
+const inputsContainer = document.querySelector(".inputs-container");
+const btnAdd = document.querySelector("#btn-add");
+
+function addNewPerson() {
+  const div = document.createElement("div");
+
+  div.classList.add("person-container");
+  div.innerHTML =
+    '<div class="input-container"><label>Nombre</label><input type="text" required /></div><div class="input-container"><label>Edad</label><input type="number" required /></div>';
+
+  inputsContainer.appendChild(div);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const personsDOM = document.querySelectorAll(".person-container");
+  const personas = [];
+
+  personsDOM.forEach((x) => {
+    const persona = {};
+
+    persona.edad = parseInt(x.querySelector("input[type=number]").value);
+    persona.nombre = x.querySelector("input[type=text]").value;
+
+    personas.push(persona);
+  });
+
+  mostrarInformacion(personas);
+}
+
 function calcularPromedio(personas) {
   const total = personas.reduce((acum, persona) => acum + persona.edad, 0);
 
@@ -7,38 +39,8 @@ function calcularPromedio(personas) {
 function mostrarInformacion(personas) {
   const promedio = calcularPromedio(personas);
 
-  console.info(` ℹ️ La edad promedio es: ${promedio}`);
-
-  personas.forEach((persona) => {
-    console.info(` 👨🏼‍🎓 ${persona.nombre}. Edad: ${persona.edad}`);
-  });
+  document.querySelector("#result").innerHTML = `La edad promedio es ${promedio}`;
 }
 
-function iniciarSimulador() {
-  const personas = [];
-  let continuar = true;
-
-  do {
-    const nombre = prompt(
-      `Ingrese nombre para la persona ${
-        personas.length + 1
-      } (Para finalizar escriba ESC)`
-    );
-
-    if (nombre.toLocaleUpperCase() === "ESC") {
-      continuar = false;
-    } else {
-      const edad = parseInt(
-        prompt(`Ingrese edad para la persona ${personas.length + 1}`)
-      );
-
-      personas.push({ nombre: nombre, edad: edad });
-    }
-  } while (continuar);
-
-  alert("Abri la consola para ver la informacion");
-
-  mostrarInformacion(personas);
-}
-
-iniciarSimulador();
+btnAdd.addEventListener("click", addNewPerson);
+form.addEventListener("submit", handleSubmit);
